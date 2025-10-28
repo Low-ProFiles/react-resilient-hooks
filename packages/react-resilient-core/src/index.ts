6e6b4a23a6c17080c0db366db235792b32eb85aa
박민철 <parkmincheol@bagmincheol-ui-MacBookPro.local>
2025-10-28 09:58:25 +0900

    refactor: move sources into core and utils packages

--- packages/react-resilient-core/src/index.ts
+++ packages/react-resilient-core/src/index.ts
@@ -1,23 +1,8 @@
 export * from './core/defaultProviders';
 export * from './core/idbUtils';
 export * from './core/indexedDBProvider';
-export * from './core/ResilientProvider';
 export * from './core/security';
 export * from './core/storageFactory';
 export * from './core/storageProvider';
 export * from './core/types';
-
-export * from './hooks/useAdaptiveImage';
-export * from './hooks/useBackgroundSync';
-export * from './hooks/useConnectionAwarePolling';
-export * from './hooks/useNetworkStatus';
-export * from './hooks/useOfflineCache';
-export * from './hooks/useOnline';
-export * from './hooks/useQueue';
-export * from './hooks/useRetry';
-export * from './hooks/useRetryRequest';
-export * from './hooks/useWebsocket';
-
-
-
-export * from './utils/registerServiceWorker';
+export * from './sw/service-worker';
